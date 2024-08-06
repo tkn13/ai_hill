544e642e45f13df6a85961abbedad874bfdd6445
moneywar <kong.deesamoot@gmail.com>
2024-08-06 20:00:07 +0700

    Add more massege to txt file and add more func

--- Main.java
+++ Main.java
@@ -6,10 +6,16 @@ public class Main {
     Puzzle8Slover p = new Puzzle8Slover("output/input50.txt");
     public static void main(String[] args) {
         Main m = new Main();
-        m.hillCalimingTest();
-        m.beamSearchTest();
-        m.BFSTest();
-        m.DFSTest();
+        for(int i=0;i<1;i++) {
+            m.hillCalimingTest();
+            m.beamSearchTest();
+            m.BFSTest();
+            m.DFSTest();
+            m.BFSLimitTest(15);
+            m.DFSLimitTest(15);
+        }
+
+        //m.complexTest();
     }
 
     public void hillCalimingTest(){
@@ -41,9 +47,10 @@ public class Main {
         double success = (double) completed / round;
         try {
             FileWriter writer = new FileWriter("stat.txt",true);
-            writer.write((endTime - startTime) + " ");
-            writer.write(success + "");
-            writer.write("\n");
+            writer.write("Hill Climbing" + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
@@ -81,9 +88,10 @@ public class Main {
         double success = (double) completed / round;
         try {
             FileWriter writer = new FileWriter("stat.txt",true);
-            writer.write((endTime - startTime) + " ");
-            writer.write(success + "");
-            writer.write("\n");
+            writer.write("Beam Search" + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
@@ -106,7 +114,7 @@ public class Main {
                 // System.out.println("Progress: " + (round / tenPercent) * 10 + "%");
                 percentCouter = 0;
             }
-            if (p.BFS(false, false, 25, round)) {
+            if (p.BFS(false, false, round)) {
                 completed++;
             }
         }
@@ -120,9 +128,50 @@ public class Main {
         double success = (double) completed / round;
         try {
             FileWriter writer = new FileWriter("stat.txt",true);
-            writer.write((endTime - startTime) + " ");
-            writer.write(success + "");
-            writer.write("\n");
+            writer.write("BFS" + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    public void BFSLimitTest(int Limit){
+        final int N = 1000;
+        int round = 0;
+        int percentCouter = 0;
+        int tenPercent = N / 10;
+        int completed = 0;
+        System.out.println("\nStart simulation Puzzle8 "+ N + " rounds");
+        long startTime = System.currentTimeMillis();
+        while (round < N) {
+            round++;
+            percentCouter++;
+            if (percentCouter == tenPercent) {
+                // System.out.println("Progress: " + (round / tenPercent) * 10 + "%");
+                percentCouter = 0;
+            }
+            if (p.BFS(false, false, Limit,round)) {
+                completed++;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) + " ms");
+        System.out.println("\nSimulation completed");
+        System.out.println("BFS Limit" + Limit);
+        System.out.println("Number of rounds: " + round);
+        System.out.println("Number of completed: " + completed);
+        System.out.println("Success rate: " + (double) completed / round);
+        double success = (double) completed / round;
+        try {
+            FileWriter writer = new FileWriter("stat.txt",true);
+            writer.write("BFS Limit " + Limit + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
@@ -145,7 +194,7 @@ public class Main {
                 // System.out.println("Progress: " + (round / tenPercent) * 10 + "%");
                 percentCouter = 0;
             }
-            if (p.DFS(false, false, round)) {
+            if (p.DFS(false, false,round)) {
                 completed++;
             }
         }
@@ -159,9 +208,10 @@ public class Main {
         double success = (double) completed / round;
         try {
             FileWriter writer = new FileWriter("stat.txt",true);
-            writer.write((endTime - startTime) + " ");
-            writer.write(success + "");
-            writer.write("\n");
+            writer.write("DFS" + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
@@ -169,6 +219,45 @@ public class Main {
         }
     }
 
+    public void DFSLimitTest(int Limit){
+        final int N = 1000;
+        int round = 0;
+        int percentCouter = 0;
+        int tenPercent = N / 10;
+        int completed = 0;
+        System.out.println("\nStart simulation Puzzle8 "+ N + " rounds");
+        long startTime = System.currentTimeMillis();
+        while (round < N) {
+            round++;
+            percentCouter++;
+            if (percentCouter == tenPercent) {
+                // System.out.println("Progress: " + (round / tenPercent) * 10 + "%");
+                percentCouter = 0;
+            }
+            if (p.DFS(false, false,Limit,round)) {
+                completed++;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) + " ms");
+        System.out.println("\nSimulation completed");
+        System.out.println("DFS");
+        System.out.println("Number of rounds: " + round);
+        System.out.println("Number of completed: " + completed);
+        System.out.println("Success rate: " + (double) completed / round);
+        double success = (double) completed / round;
+        try {
+            FileWriter writer = new FileWriter("stat.txt",true);
+            writer.write("DFS Limit " + Limit + "\n");
+            writer.write("Time: " + (endTime - startTime) + " ms" + " ");
+            writer.write("Success rate: " + success + "");
+            writer.write("\n\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
 
     public void complexTest(){
         Puzzle8Slover complex = new Puzzle8Slover("input.txt");
